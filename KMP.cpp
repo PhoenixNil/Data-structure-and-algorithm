31f19d697725c5e848194ff14d39d1c9cfa8fe35
俊皓 周 <1270128439@qq.com>
2021-04-10 02:25:26 +0800

    11

--- KMP.cpp
+++ KMP.cpp
@@ -17,6 +17,8 @@ int *compute_prefix(string P) //计算模式P的前缀，下一步的偏移量�
             k = k + 1;
         pi[q] = k; //这个是把算的k的值（就是相同的最大前缀和最大后缀长）赋给pi[q]
     }
+    //找到最长公共子串的目的，什么是最长公共子串呢？就是类似于 ABCBA这种，从前往后，和从后往前都是ABC或者ABEAEE,对于最后一个E来说，他前面的串
+    //是ABEAE，从前往后，和从后往前没有一个相同的，因为第一个就不同了，所以他的最长公共子串为0
     return pi;
 }
 
@@ -24,6 +26,7 @@ void KMP(string T, string P) //string在c++是类
 {
     int n, m, q, i;
     int *pi;
+    int flag;
     n = T.length();
     m = P.length();
     pi = compute_prefix(P); //计算前缀函数，即接下来的偏移量
@@ -36,10 +39,16 @@ void KMP(string T, string P) //string在c++是类
             q = q + 1;
         if (q == m) //全部匹配
         {
+            flag = 1;
             cout << "shift " << i - m + 1 << endl;
             q = pi[q - 1]; //找下一个匹配值(不能大于m)
         }
     }
+    if (!flag)
+    {
+        cout << "no find";
+    }
+
     delete (pi);
 }
 
